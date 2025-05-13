419355bad0f9d3093ae8196b824ff993209f22b3
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-05-13 18:10:31 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,6 +5,14 @@ st.set_page_config(
     page_icon="👋",
 )
 
+pages_manager = st.navigation(
+    [
+        st.Page("pages/plot.py"),
+        st.Page("pages/map.py"),
+    ]
+    )
+
+pages_manager.run()
 st.write("# Welcome to Streamlit! 👋")
 
 st.sidebar.success("Select a demo above.")
